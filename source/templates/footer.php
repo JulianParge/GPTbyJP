d333ffecbc03246c6aba81b4c0dab72020d530cf
Julian Parge <jp@sqy.re>
2023-07-16 17:32:53 +0100

    Wrapped the body into a container.

--- source/templates/footer.php
+++ source/templates/footer.php
@@ -1,4 +1,4 @@
-
+</div>
 
 <script src="https://kit.fontawesome.com/e6a4838327.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
