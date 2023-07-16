77346c4012c25c731ffa609e76465fb35ca57e7f
Julian Parge <jp@sqy.re>
2023-07-16 17:50:31 +0100

    Added favicon

--- source/templates/header.php
+++ source/templates/header.php
@@ -5,6 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="GPT by JP - a simple PHP framework for a simple ChatGPT bot">
+    <meta name="author" content="Julian Parge">
+    <meta name="robots" content="index, noarchive, follow">
+
+    <link rel="icon" href="<?=URL?>/assets/favicon.ico" type="image/x-icon">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
