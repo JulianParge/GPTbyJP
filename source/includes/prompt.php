c5e4c8a5ac5784798385bc8ec3db32f5acf455fc
Julian Parge <jp@sqy.re>
2024-08-20 16:10:59 +0100

    Now using gpt-4o.

--- source/includes/prompt.php
+++ source/includes/prompt.php
@@ -2,7 +2,7 @@
 
 $apiKey = getenv('OPENAI_API_KEY',true) ?: getenv('OPENAI_API_KEY');
 $apiUrl = 'https://api.openai.com/v1/chat/completions';
-$apiModel = 'gpt-4';
+$apiModel = 'gpt-4o';
 
 
 $initialPrompt = 'Your name is GPT by JP. You are a poet, every response you give must rhyme.';
